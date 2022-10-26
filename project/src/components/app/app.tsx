bd2da4fb93c6004b85c62434e7e2a7023d556920
Leonov-nic <leonov-nic@yandex.ru>
2022-10-26 19:23:17 +0300

    настройка маршрутизации

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,10 @@
 import MainPage from '../../pages/main-page/main-page';
+import NoPages from '../../pages/no-pages/no-pages';
+import LoginPage from '../../pages/login-page/login-page';
+import RoomPage from '../../pages/room-page/room-page';
+import PrivateRoute from '../private-route/private-route';
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
 
 type AppScreenProps = {
   amountCards: number;
@@ -6,7 +12,24 @@ type AppScreenProps = {
 
 function App({amountCards}: AppScreenProps): JSX.Element {
   return (
-    <MainPage amountCards={amountCards}/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<MainPage amountCards={amountCards}/>} />
+        {/* <Route path={AppRoute.Root} element={<MainPage amountCards={amountCards}/>} /> */}
+        {/* <Route path={AppRoute.Room} element={<RoomPage />} /> */}
+        <Route path={`${AppRoute.Room}/:id`} element={<RoomPage />} />
+        <Route path={AppRoute.Login} element={<LoginPage />} />
+        <Route path="*" element={<NoPages />} />
+        <Route
+          path={AppRoute.Room}
+          element={
+            <PrivateRoute authorizationStatus={AuthorizationStatus.Auth}>
+              <RoomPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Main = '/main',
+  Login = '/login',
+  Room = '/room',
+  Root = '/',
+}
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
