c9edc9e7b7b77c8dff244d87d8f4088249560d2b
Leonov-nic <leonov-nic@yandex.ru>
2022-12-10 20:51:26 +0300

    исправляет значения defaultValue на value

--- project/src/components/comment-form/comment-form.tsx
+++ project/src/components/comment-form/comment-form.tsx
@@ -54,7 +54,7 @@ export default function CommentForm({hotelId}:CommentFormProps): JSX.Element {
       <div className="reviews__rating-form form__rating">
         {ratingLevels.map((level, index) => <RatingInputRadio rating={rating} index={5 - index} title={level} handleInputClick={handleInputClick} key={level} />)}
       </div>
-      <textarea ref={textRef} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" defaultValue={reviewText} onChange={handleTextareaChange}></textarea>
+      <textarea ref={textRef} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved" value={reviewText} onChange={handleTextareaChange}></textarea>
       <div className="reviews__button-wrapper">
         <p className="reviews__help">
           To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
--- project/src/components/rating-input-radio/rating-input-radio.tsx
+++ project/src/components/rating-input-radio/rating-input-radio.tsx
@@ -8,7 +8,7 @@ type RatingInputRadioProps = {
 export default function RatingInputRadio({rating, index, title, handleInputClick}:RatingInputRadioProps): JSX.Element {
   return (
     <>
-      <input className="form__rating-input visually-hidden" name="rating" defaultValue={index} id={`${index}-stars`} type="radio" onClick={handleInputClick} defaultChecked={rating === index}/>
+      <input className="form__rating-input visually-hidden" name="rating" defaultValue={index} id={`${index}-stars`} type="radio" onClick={handleInputClick} checked={rating === index}/>
       <label htmlFor={`${index}-stars`} className="reviews__rating-label form__rating-label" title={title}>
         <svg className="form__star-image" width="37" height="33">
           <use xlinkHref="#icon-star"></use>
